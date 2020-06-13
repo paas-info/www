9a3e93851561f27ef671f8851f2f382a7c9b63b3
tom <tom@sapletta.com>
2020-06-13 16:19:51 +0200

    implementation of deployment

--- js/flowtype.js
+++ js/flowtype.js
@@ -1,7 +1,9 @@
-$('body').flowtype({
-    minimum   : 600,
-    maximum   : 900,
-    minFont   : 15,
-    maxFont   : 25,
-    fontRatio : 10
+$(function () {
+    $('body').flowtype({
+        minimum: 600,
+        maximum: 900,
+        minFont: 15,
+        maxFont: 25,
+        fontRatio: 10
+    });
 });
